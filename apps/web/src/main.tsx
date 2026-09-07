8642426907d9c892acf98283e068eaedbd7e7147
JonathanBlackDoctor <alpaomegastartend@gmail.com>
2026-09-08 01:21:44 +0900

    feat(ui): load classic skin overrides

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './classic-theme.css';
 import App from './App';
 import { startPrayerTextMigration } from './features/prayers/prayerTextMigration';
 
